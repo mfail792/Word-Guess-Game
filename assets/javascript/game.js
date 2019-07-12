2263c9c3b9db08d7beb0187f6a21ef8fef2ed6a5
Magus <mfail792@gmail.com>
2019-07-12 01:06:39 -0500

    starting out

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var randomPlanet = ["Neptune", "Venus", "Uranus", "Mars", "Jupiter", "Earth", "Saturn", "Mercury"];
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+
+// key event to start the game
