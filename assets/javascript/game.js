4ab93279fbd6e476100d0e5500d7240d49954940
Magus <mfail792@gmail.com>
2019-07-12 21:25:39 -0500

    added image array to JS file, added random planet img script to HTML with some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 //defining variables
 var randomPlanet = ["Neptune", "Venus", "Uranus", "Mars", "Jupiter", "Earth", "Saturn", "Mercury"];
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var random_images_array = 
+var random_images_array = [".assets/images/Earth.jpg", ".assets/images/Jupiter.jpg", ".assets/images/Mars.jpg", ".assets/images/Neptune.jpg", "./assets/images/Saturn.jpg", ".assets/images/Venus.jpg"]
 
 // event to start the game upon key press by calling on the body element
 document.getElementById("body").addEventListener("onclick", getRandomImage(imgAr, path)
